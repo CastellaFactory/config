617c8629096858ec9d03d36274daa8b6f7d55a74
CastellaFactory <castellafactory@gmail.com>
2017-05-04 04:14:13 +0900

    avoid full path

--- fish/functions/peco_select_ghq_repository.fish
+++ fish/functions/peco_select_ghq_repository.fish
@@ -5,10 +5,10 @@ function peco_select_ghq_repository
         set peco_flags --query "$query"
     end
 
-    ghq list -p | peco $peco_flags | read line
+    ghq list | peco $peco_flags | read line
 
     if [ $line ]
-        cd $line
+        cd (ghq root)/$line
         commandline -f repaint
     end
 end
