0abf714477dd156fe3673be7e08486eb40d8f43b
CastellaFactory <castellafactory@gmail.com>
2024-01-28 17:49:59 +0900

    disable ctrl-n, ctrl-w

--- wezterm/dot.config/wezterm/keymapping.lua
+++ wezterm/dot.config/wezterm/keymapping.lua
@@ -6,16 +6,6 @@ local cmd_or_ctrl = platform.is_mac and 'CMD' or 'CTRL'
 return {
     setup = function(config)
         config.keys = {
-            {
-                key = 't',
-                mods = cmd_or_ctrl,
-                action = wezterm.action.SpawnTab 'CurrentPaneDomain',
-            },
-            {
-                key = 'w',
-                mods = cmd_or_ctrl,
-                action = wezterm.action.CloseCurrentTab { confirm = true },
-            },
             { key = 'F1', mods = 'NONE', action = 'ActivateCopyMode' },
             { key = 'F2', mods = 'NONE', action = wezterm.action.ActivateCommandPalette },
             { key = 'F3', mods = 'NONE', action = wezterm.action.ShowLauncher },
