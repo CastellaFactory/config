a08207168c8df6b8a947f5633d35018605be0103
CastellaFactory <castellafactory@gmail.com>
2026-01-01 09:52:39 +0900

    update

--- wezterm/dot.wezterm.lua
+++ wezterm/dot.wezterm.lua
@@ -6,6 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+require('core').setup(config)
 require('fontsetting').setup(config)
 require('appearance').setup(config)
 require('keymapping').setup(config)
